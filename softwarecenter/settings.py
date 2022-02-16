5330e294e6fb6fccfe5b6afb11b2f9a631248cca
abel <cyjmmy@foxmail.com>
2022-02-16 21:02:58 +0800

    bbb

--- softwarecenter/settings.py
+++ softwarecenter/settings.py
@@ -73,10 +73,10 @@ WSGI_APPLICATION = 'softwarecenter.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 # Password validation
